887a9b0f2caef9d8e2f71474371fe9e87295cbe4
Sofia Pereira <73718358+SofiaPereira18@users.noreply.github.com>
2025-01-06 20:50:36 +0000

    feito até a etapa computed() e truncate description

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobsListings from '@/components/JobsListings.vue';
+
+</script>
+
 <template>
   <h1 class="text-2xl">Vue Jobs</h1>
-
+  <NavBar />
+  <Hero />
+  <HomeCards />
+  <JobsListings :limit="3" :showButton="true"/>
 </template>
