e047e4a2e4163b308c08a20167974908fa0c4698
Sofia Pereira <73718358+SofiaPereira18@users.noreply.github.com>
2025-01-11 20:46:10 +0000

    TUTORIAL DONE

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from '@/components/HomeCards.vue';
-import JobsListings from '@/components/JobsListings.vue';
+import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
   <NavBar />
-  <Hero />
-  <HomeCards />
-  <JobsListings :limit="3" :showButton="true"/>
+  <RouterView />
 </template>
